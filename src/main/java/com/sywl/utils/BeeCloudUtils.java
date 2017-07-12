4cecc60688a7011d81cda3876c5a895f002996a9
294889981@qq.com <px294889981>
2017-07-12 19:52:25 +0800

    提交工具类

---
+++ src/main/java/com/sywl/utils/BeeCloudUtils.java
@@ -0,0 +1,13 @@
+package com.sywl.utils;
+
+
+
+/**
+ * Created by Administrator on 2017/7/11.
+ */
+public class BeeCloudUtils {
+    public static final String BEECLOUD_URL = "https://api.beecloud.cn";
+    public static final String BEECLOUD_PAY_PAHT = "/2/rest/bill";
+
+
+}
